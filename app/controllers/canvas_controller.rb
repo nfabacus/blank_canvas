4acd8ae894bdd2592cdea21e0d14df1135ed709e
benjamin-white <benjaminwhite@live.co.uk>
2016-09-08 14:52:43 +0100

    adds vector to show page

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -10,12 +10,13 @@ class CanvasController < ApplicationController
   end
 
   def show
-    @canva = Canva.first
+    @choice = params[:selection]
+    @canva = Canva.find(params[:id])
   end
 
   def create
     @canva = Canva.create(canva_params)
-    redirect_to "/canvas/#{@canva.id}"
+    redirect_to "/canvas/#{@canva.id}?selection=#{params[:selection]}"
   end
 
   private
