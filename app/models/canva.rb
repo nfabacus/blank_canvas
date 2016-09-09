cbc01d97eb3c1991830a9683ae5812f72d0bd2ca
noby <n.fujioka@abacusmaths.info>
2016-09-09 13:07:22 +0100

    testing data-x and data-y

--- app/models/canva.rb
+++ app/models/canva.rb
@@ -1,6 +1,6 @@
 class Canva < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :large => "900x900>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => {  :medium => "300x300>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_presence :image
 
