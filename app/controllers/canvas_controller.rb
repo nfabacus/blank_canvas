096c8cee5ad2a08e1cb7a246301ef3124295fa7f
hannako <jessjones1@gmail.com>
2016-09-14 12:05:15 +0100

    reinstates update method

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -47,7 +47,20 @@ class CanvasController < ApplicationController
     @color = create_palette
   end
 
-  
+  def update
+    @svg = params[:room_choice]
+    @canva = Canva.find(params[:id])
+    check_for_param_presence
+    if @canva.save && valid_photo?
+      if user_signed_in?
+        current_user.canva << @canva
+      end
+      redirect_to canva_path(@canva, room_choice: @svg)
+    else
+      flash[:notice] = "Please select a valid picture"
+      redirect_to new_canva_path(room_choice: @svg)
+    end
+  end
 
   def paint
       @canva = Canva.find(params[:canva_id])
