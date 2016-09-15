7638a8d90d878968dbc83e0ea6025e974c0ae5ec
hannako <jessjones1@gmail.com>
2016-09-15 14:45:13 +0100

    fixes 15 tests, 2 still bust. deletes update method from controller as no longer used, and also removes two modules, direct me wisely and update params, as these now no longer needed. updates css of the error message. creates new spec test for sketch.

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -6,12 +6,12 @@ end
 def create_a_coloured_canvas
   visit '/'
   click_link 'sign in'
-  fill_in 'Username', with: 'test_user'
-  fill_in 'Email', with: 'test@test.co.uk'
-  fill_in 'Password', with: '123456'
-  fill_in 'Password confirmation', with: '123456'
+  fill_in 'username', with: 'test_user'
+  fill_in 'email', with: 'test@test.co.uk'
+  fill_in 'password', with: '123456'
+  fill_in 'password confirmation', with: '123456'
   click_button 'Sign up'
   click_button('first_svg')
-  page.attach_file "canva_image", Rails.root + "spec/assets/black2.jpg"
-  click_button "Show my colours"
+  page.attach_file "canva[image]", Rails.root + "spec/assets/black2.jpg"
+  click_button "Show my Colours"
 end
