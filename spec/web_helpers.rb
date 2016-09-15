95155f5aaaf6967e3640cfdfd6caea3aa52452b2
hannako <jessjones1@gmail.com>
2016-09-15 15:42:15 +0100

    deletes duplicate devise views. All tests passing. NBG

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -5,11 +5,11 @@ end
 
 def create_a_coloured_canvas
   visit '/'
-  click_link 'sign in'
-  fill_in 'username', with: 'test_user'
-  fill_in 'email', with: 'test@test.co.uk'
-  fill_in 'password', with: '123456'
-  fill_in 'password confirmation', with: '123456'
+  click_link 'sign up'
+  fill_in 'user[user_name]', with: 'test_user'
+  fill_in 'user[email]', with: 'test@test.co.uk'
+  fill_in 'user[password]', with: '123456'
+  fill_in 'user[password_confirmation]', with: '123456'
   click_button 'Sign up'
   click_button('first_svg')
   page.attach_file "canva[image]", Rails.root + "spec/assets/black2.jpg"
