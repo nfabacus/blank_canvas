30d40a68b83930e13ccb2ca79937a36fc048015f
noby <n.fujioka@abacusmaths.info>
2016-10-05 23:15:09 +0100

    installed pure css files

--- app/models/canva.rb
+++ app/models/canva.rb
@@ -3,7 +3,7 @@ class Canva < ActiveRecord::Base
 
   has_attached_file :image, :styles => {  :medium => "300x300>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage/
-  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /jpg\Z/, /JPG\Z/, /gif\Z/]
   validates_attachment_presence :image
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 5.megabytes
 
